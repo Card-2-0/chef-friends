355c160276697bb03e0a1a36bfb96b9f940ced22
Kaousheik <kaousheik@gmail.com>
2020-06-01 12:07:44 +0530

    styled reg

--- web/src/components/Register.tsx
+++ web/src/components/Register.tsx
@@ -1,38 +1,30 @@
 import React from "react";
-import { Input, Form, Button } from "antd"
+import { Input, Form, Button, Typography } from "antd";
 import { useCreateUserMutation } from "../generated";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
 export const Register = () => {
-  const [form] = Form.useForm() 
-  const ivalue = (id: string) =>
-    (document.getElementById(id) as HTMLInputElement).value;
+  const [form] = Form.useForm();
   const [
     createUserMutation,
     { data, error, loading },
   ] = useCreateUserMutation();
-  const handlesSubmit = () => {}
-
-  const submit = async () => {
-    const name = ivalue("name");
-    const email = ivalue("email");
-    const userid = ivalue("userid");
-    const pw1 = ivalue("pw1");
-    const pw2 = ivalue("pw2");
-    if (pw1 !== pw2 || pw1.length <6) {
-      alert("Passwords must be atleast 6 characters long and both must match, please check");
-      return;
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      console.log(values);
+      createUserMutation({
+        variables: {
+          name: values["name"],
+          email: values["email"],
+          password: values["password"],
+          userid: values["codechefID"],
+        },
+      });
+    } catch (err) {
+      console.log(err);
     }
-
-    await createUserMutation({
-      variables: {
-        name,
-        email,
-        userid,
-        password: pw1,
-      },
-    });
   };
   console.log(error);
   console.log(data);
@@ -40,90 +32,73 @@ export const Register = () => {
     if (data.createUser === "1") {
       alert("Given codechef ID does not exist, please check");
     } else if (data.createUser === "2") {
-      alert("Email ID already exists")
+      alert("Email ID already exists");
     } else if (data.createUser !== "3") {
       localStorage.setItem("chef", data.createUser);
       window.location.pathname = "user";
     }
-    data.createUser = "3"
+    data.createUser = "3";
   }
 
   return (
-    <div>
-      <h1>Welcome to ChefBook !</h1>
-      <p> Please fill details to register yourself </p>
-      <p> Given Username now can't be changed later </p>
-      <Form form={form} onFinish={handlesSubmit}>
-        <div>
-        <Form.Item
-          name = "name"
-          label = "name"
-          rules={[{ required: true, message: "Name is required!" }]}>
-          <Input placeholder="Enter Name" type="text" />
-        </Form.Item>
-        <Form.Item
-          name = "email"
-          label = "email"
-          rules={[
-            { required: true, message: "Email is required!" },
-            { pattern: emailRegex, message: "Enter Valid Email" },
-          ]}>
-          <Input placeholder="Enter Email" type="email" />
-        </Form.Item>
-        <Form.Item
-            name = "ccID"
-            label = "CC ID">
-            <Input placeholder="Enter CodeChef ID" type="text" />
-        </Form.Item>
-        <Form.Item
-          name = "password"
-          label = "password">
-          rules={[
-            { min: 8, message: "Enter Valid Password (atleast 8 characters)" },
-            { required: true, message: "Password is required!" },
-          ]}
-          <Input placeholder="Enter Password" type="password" />
-        </Form.Item>
-        <Form.Item
-            name="passwordRe"
-            label="Re Enter Password"
-            rules={[
-              { min: 8, message: "Enter Valid Password" },
-              { required: true, message: "Password is required!" },
-              {},
-            ]}>
-            <Input placeholder="Enter Password" type="password" />
-          </Form.Item>
-          <Form.Item>
-            <Button
-              htmlType="submit"
-              type="primary"
-              loading={loading}
-              block
-            >
-              Submit
-            </Button>
-          </Form.Item>
-          </div>
-      </Form>
-      <form>
-      <input id="name" placeholder="Enter Name"></input>
-      <input
-        id="email"
-        placeholder="Enter Email"
-        type="email"
-        pattern="[^ @]*@[^ @]*"
-      ></input>
-      <input id="userid" placeholder="Enter Codechef ID"></input>
-      <input id="pw1" placeholder="Enter password" type="password"></input>
-      <input id="pw2" placeholder="Re-enter password" type="password"></input>
-      </form>
-      <button onClick={submit} disabled={loading}>
+    <Form form={form} onFinish={handleSubmit}>
+      <Typography>
         {" "}
-        Submit{" "}
-      </button>
-      <h5>Or Login</h5>
-      <Link to="login">HERE</Link>
-    </div>
+        Welcome to chef ChefBook
+        <Typography>PLease fill details to register</Typography>
+        <Typography>Given username can't be changed</Typography>
+      </Typography>
+      <Form.Item
+        name="name"
+        label="name"
+        required
+        rules={[
+          {
+            required: true,
+            message: "Name required",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="email"
+        required
+        rules={[
+          {
+            required: true,
+            message: "Email required",
+          },
+          { pattern: emailRegex, message: "Enter Valid Email" },
+        ]}
+      >
+        <Input type="email" />
+      </Form.Item>
+      <Form.Item
+        name="codechefID"
+        label="codechef Id"
+        required
+        rules={[
+          {
+            required: true,
+            message: "Code chef id reqiured",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name="password" label="password">
+        <Input.Password />
+      </Form.Item>
+      <Form.Item name="confirm-password" label="confirm password">
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
